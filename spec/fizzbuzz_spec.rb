6aafa71a33158e1d2c7492767bdbe46a59994447
Robert <r_lee94@yahoo.com>
2019-03-14 17:17:30 +0000

    Automatically backed up by Learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,13 +1,13 @@
 
-def fizzBuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    "FizzBuzz"
-  elsif number % 3 == 0
-    "Fizz"
-  elsif number % 5 == 0
-    "Buzz"
-  else
-    number
+#def fizzBuzz(number)
+ # if number % 3 == 0 && number % 5 == 0
+   # "FizzBuzz"
+ # elsif number % 3 == 0
+  #  "Fizz"
+ # elsif number % 5 == 0
+  #  "Buzz"
+ # else
+   # number
   end
 end
 
